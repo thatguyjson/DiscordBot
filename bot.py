d0dd3840b5235bbb40c3cb56f889ac5791cd799b
Json <jasonarodriguez16@gmail.com>
2025-02-16 18:19:03 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -125,7 +125,9 @@ async def readme(ctx):
 async def refresh_cookie_data():
     global data, cookies, startDate, endDate
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     try:
         driver.get('https://crumblcookies.com')
